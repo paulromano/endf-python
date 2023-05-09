63cdd46232536de785f2f70918b14e9e50fa1806
Paul Romano <paul.k.romano@gmail.com>
2023-05-08 23:39:01 -0500

    Add stub for documentation

---
+++ doc/source/conf.py
@@ -0,0 +1,44 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'endf'
+copyright = '2023, International Atomic Energy Authority'
+author = 'Paul Romano'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_autodoc_typehints',
+    'sphinx_design',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'pydata_sphinx_theme'
+html_static_path = ['_static']
+
+html_theme_options = {
+    "github_url": "https://github.com/paulromano/endf",
+    #"navbar_end": ["navbar-icon-links"],
+    "show_toc_level": 3,
+}
---
+++ doc/source/index.rst
@@ -0,0 +1,20 @@
+.. endf documentation master file, created by
+   sphinx-quickstart on Mon May  8 23:24:32 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to endf's documentation!
+================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
