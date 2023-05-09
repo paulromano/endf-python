27a42402e15d1fc9aab2c957d07f818dca095596
Paul Romano <paul.k.romano@gmail.com>
2023-05-08 21:10:23 -0500

    Change project name to 'endf'

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from pybind11.setup_helpers import build_ext, intree_extensions
 
 
-ext_modules = intree_extensions(["src/endfpy/_records.cpp"])
+ext_modules = intree_extensions(["src/endf/_records.cpp"])
 
 setup(
     ext_modules=ext_modules,
