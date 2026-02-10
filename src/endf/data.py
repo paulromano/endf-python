269889f2c512ab400ad5d483f5cc2b6aa5c40057
shimwell <mail@jshimwell.com>
2026-02-10 20:25:58 +0100

    match to fullmatch like openmc

--- src/endf/data.py
+++ src/endf/data.py
@@ -119,7 +119,7 @@ def zam(name: str) -> Tuple[int, int, int]:
 
     """
     try:
-        symbol, A, state = _GNDS_NAME_RE.match(name).groups()
+        symbol, A, state = _GNDS_NAME_RE.fullmatch(name).groups()
     except AttributeError:
         raise ValueError(f"'{name}' does not appear to be a nuclide name in "
                          "GNDS format")
